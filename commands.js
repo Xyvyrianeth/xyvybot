e5e6bc99f52626080d488f735bbf14eb135d4f12
Xyvyrianeth <strobinsson@gmail.com>
2018-08-09 17:11:20 -0500

    Revert "More testing"
    This reverts commit c19fe3493e77f94e9d0f4c92e0fae75d4564502f.

--- commands.js
+++ commands.js
@@ -1,11 +1,10 @@
-var version = "2.24.0.3";
+var version = "2.24.0.2";
 
 const Discord = require("discord.js");
 const Canvas = require("canvas");
 const Jimp = require("jimp");
 const GIFEncoder = require("gifencoder");
 const streamBuffers = require('stream-buffers');
-const server = require('./server.js');
 
 var client = require("./Xyvy.js").client;
 var config = require("./Xyvy.js").config;
