1a4148597ce4f374cf23f6e4f246f7639f93a836
Xyvyrianeth <strobinsson@gmail.com>
2018-08-14 08:06:21 -0500

    Update gomoku.js

--- games/gomoku.js
+++ games/gomoku.js
@@ -5,7 +5,7 @@ var gamename = "Gomoku";
 var shortname = "gomoku";
  
 exports.newGame = function(channel, player1, cmd, mode) {
-    channels[channel.id] = {game:shortname,channel:channel,turn:0,players:[],started:false,height:10,width:10,lastmove:'',player:false,RE:/^([a-z] ?[0-9]{1,}|[0-9]{1,} ?[a-z])/i,casual:mode};
+    channels[channel.id] = {game:shortname,channel:channel,turn:0.5,players:[],started:false,height:10,width:10,lastmove:'',player:false,RE:/^([a-z] ?[0-9]{1,}|[0-9]{1,} ?[a-z])/i,casual:mode};
     let game = channels[channel.id];
     let _ = false;
     game.board = [];
@@ -36,7 +36,7 @@ exports.startGame = function(channel, player2) {
  
     game.players = (Math.random() * 2 | 0) == 0 ? game.players : [game.players[1], game.players[0]]; // Makes player one random instead of always the challenger
     game.player = game.players[0];
- 
+
     return ["The game has started! <@" + game.players[0] + "> will be player1, and <@" + game.players[1] + "> will be player2!\n\nTo place a piece, just say the number of the column you wish to place in, like \"f4\"\nThe board starts out as 10x10, and as more room is needed, the board will expand. The official rules for Gomoku state that there is no legal size limit for the board, so the board will expand to my own physical capabilities, but I'm gonna choose to stop expanding at 26x26.", new Discord.Attachment(exports.drawBoard(game, 0), `${shortname}_0_${game.players[0]}vs${game.players[1]}.png`)];
 }
  
@@ -346,7 +346,7 @@ exports.nextTurn = function(channel, end, highlight) {
     let game = channels[channel.id];
     if (end == 0) {
         game.turn = game.turn == 1.5 ? 0 : game.turn += 0.5;
-        game.player = game.players[game.turn];
+        game.player = game.players[Math.floor(game.turn)];
     }
     game.buffer = exports.drawBoard(game, end, highlight);
     board = new Discord.Attachment(game.buffer, end == 1 ? `${shortname}_${end}_${game.players[game.turn]}.png` : `${shortname}_${end}_${game.players[0]}vs${game.players[1]}.png`);
