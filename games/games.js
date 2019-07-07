1ca9448af431cf57255e89463999346a530f821c
Xyvyrianeth <strobinsson@gmail.com>
2019-07-07 01:44:20 -0500

    Test

--- games/games.js
+++ games/games.js
@@ -2,11 +2,10 @@ const Discord = require("discord.js");
 const { client, config } = require("/app/Xyvy.js");
 var db = require("/app/commands.js").db;
 
-var games = []; // Leave blank
-
 var timer = setInterval(function() {
+    let games = exports.games;
     console.log(exports.games);
-    exports.games.forEach((game, index) => {
+    games.forEach((game, index) => {
         game.timer.time -= 1;
         if (game.timer.time == 0)
         {
@@ -15,7 +14,7 @@ var timer = setInterval(function() {
                 client.channels.get(ch).send(game.timer.message, game.buffer);
             }
             delete games[index];
-            exports.games.splice(index, 1);
+            games.splice(index, 1);
         }
         if (game.forfeit)
         {
@@ -24,18 +23,18 @@ var timer = setInterval(function() {
                 client.channels.get(ch).send(`Well, <@${game.forfeit == game.players[0] ? game.players[1] : game.players[0]}>, It looks like your opponent, <@${game.forfeit}>, has forfeit the game!`, {});
             }
             delete games[index];
-            exports.games.splice(index, 1);
+            games.splice(index, 1);
         }
         if (game.over)
         {
             delete games[index];
-            exports.games.splice(index, 1);
+            games.splice(index, 1);
         }
     });
 
-    if (exports.games.length > 0)
+    if (games.length > 0)
     {
-        db.query(`UPDATE games SET data = '${JSON.stringify(exports.games)}'`);
+        db.query(`UPDATE games SET data = '${JSON.stringify(games)}'`);
     }
     else
     {
@@ -46,7 +45,7 @@ var timer = setInterval(function() {
             }
             if (res.rows[0].backup)
             {
-                exports.games = res.rows[0].data;
+                games = res.rows[0].data;
                 db.query("UPDATE games SET backup = false");
             }
             else
@@ -55,6 +54,7 @@ var timer = setInterval(function() {
             }
         });
     }
+    exports.games = games;
 }, 5000);
 
-exports.games = games;
+exports.games = [];
