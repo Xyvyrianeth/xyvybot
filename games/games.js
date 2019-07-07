d5c828570195c07f2be3cfb220507909741d3194
Xyvyrianeth <strobinsson@gmail.com>
2019-07-07 01:10:22 -0500

    Maybe.... Please

--- games/games.js
+++ games/games.js
@@ -5,8 +5,7 @@ var db = require("/app/commands.js").db;
 var games = []; // Leave blank
 
 var timer = setInterval(function() {
-    console.log(exports.games);
-    games.forEach((game, index) => {
+    exports.games.forEach((game, index) => {
         game.timer.time -= 1;
         if (game.timer.time == 0)
         {
@@ -15,7 +14,7 @@ var timer = setInterval(function() {
                 client.channels.get(ch).send(game.timer.message, game.buffer);
             }
             delete games[index];
-            games.splice(index, 1);
+            exports.games.splice(index, 1);
         }
         if (game.forfeit)
         {
@@ -24,18 +23,18 @@ var timer = setInterval(function() {
                 client.channels.get(ch).send(`Well, <@${game.forfeit == game.players[0] ? game.players[1] : game.players[0]}>, It looks like your opponent, <@${game.forfeit}>, has forfeit the game!`, {});
             }
             delete games[index];
-            games.splice(index, 1);
+            exports.games.splice(index, 1);
         }
         if (game.over)
         {
             delete games[index];
-            games.splice(index, 1);
+            exports.games.splice(index, 1);
         }
     });
 
-    if (games.length > 0)
+    if (exports.games.length > 0)
     {
-        db.query(`UPDATE games SET data = '${JSON.stringify(games)}'`);
+        db.query(`UPDATE games SET data = '${JSON.stringify(exports.games)}'`);
     }
     else
     {
@@ -46,7 +45,7 @@ var timer = setInterval(function() {
             }
             if (res.rows[0].backup)
             {
-                games = res.rows[0].data;
+                exports.games = res.rows[0].data;
                 db.query("UPDATE games SET backup = false");
             }
             else
