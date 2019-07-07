6164c163dca2c50c1679444a9e5aaaed2497f53a
Xyvyrianeth <strobinsson@gmail.com>
2019-07-07 16:00:04 -0500

    Fixed blankgame.js as well

--- games/blankGame.js
+++ games/blankGame.js
@@ -1,73 +1,127 @@
 const Discord = require("discord.js");
 const Canvas = require("canvas");
-const { channels } = require("/app/games/channels.js");
+const { games } = require("/app/games/games.js");
+const { client } = require("/app/Xyvy.js");
 var gamename = "";
 var shortname = "";
+  
+exports.newGame = function(channel, player) {
+    let game = {
+        buffer: {},
+        channels: {},
+        forfeit: false,
+        game: shortname,
+        highlight: false,
+        lastmove: '',
+        over: false,
+        player: false,
+        players: [player],
+        score: [0, 0],
+        started: false,
+        turn: 0
+    };
+    game.channels[channel] = [];
+    games.push(game);
 
-exports.newGame = function(channel, player1, cmd, mode) {
-    channels[channel.id] = {game:shortname,guild:channel.guild.id,turn:0,players:[],started:false,lastmove:'',player:false,RE:/stuff/,casual:mode};
-    let game = channels[channel.id];
     game.board = [];
- 
+  
     game.timer = {
-        time: 100 * 60 * 15,
-        message: "It appears nobody wants to play right now, <@" + player1 + ">."
+        time: 900,
+        message: `It appears nobody wants to play right now, <@${player}>.`
     }
- 
-    game.players[0] = player1;
-    return `**$user$** is now requesting a new game of ${gamename}, say \`x!${cmd} start\` to play against them!`;
+
+    exports.say(game.channels, [`<@${player}> is now requesting a new game of ${gamename}!`, game.buffer]);
 }
- 
-exports.startGame = function(channel, player2) {
-    let game = channels[channel.id];
+  
+exports.startGame = function(channel1, channel2, player2) {
+    let game = games.filter(game => game.channels.hasOwnProperty(channel1))[0];
+    if (channel1 !== channel2) game.channels[channel2] = [];
     game.players[1] = player2;
     game.started = true;
- 
-    game.timer = {
-        time: 100 * 60 * 5,
-        message: "Whoops, it looks like <@" + game.players[0] + "> has run out of time, so the game is over!"
-    }
- 
-    game.players = (Math.random() * 2 | 0) == 0 ? game.players : [game.players[1], game.players[0]]; // Makes player one random instead of always the challenger
+  
+    if ((Math.random() * 2 | 0) == 0) game.players.push(game.players.shift()); // Makes player one random instead of always the challenger
     game.player = game.players[0];
- 
-    return ["The game has started! <@" + game.players[0] + "> will be player1, and <@" + game.players[1] + "> will be player2!\n\nTo place a piece, just say the number of the column you wish to place in.", new Discord.Attachment(exports.drawBoard(game, 0), `${shortname}_0_${game.players[0]}vs${game.players[1]}.png`)];
-}
- 
-exports.drawBoard = function(game, end, highlight) {
-    let canvas = new Canvas.createCanvas(220, 225);
-    let ctx = canvas.getContext("2d");
-     
-    // Function will vary with game
+  
+    game.timer = {
+        time: 600,
+        message: `Whoops, it looks like <@${game.players[0]}> has run out of time, so the game is over!`
+    }
     
-    //
-     
+    game.buffer = new Discord.Attachment(exports.drawBoard(game, 0, false), `${shortname}_0_${game.players[0]}vs${game.players[1]}.png`);
+    exports.say(game.channels, [`The game has started! <@${game.players[0]}> will be dark, and <@${game.players[1]}> will be light!`, game.buffer]);
+}
+  
+exports.drawBoard = function(game, end, highlight) {
+    let canvas = new Canvas.createCanvas(280, 300);
+    let ctx = canvas.getContext('2d');
+      
+    ctx.drawImage(exports.Images.board, 0, 0);
+
+    // ....
+
     return canvas.toBuffer();
 }
- 
-exports.takeTurn = function(channel, move) {
-    let game = channels[channel.id];
-     
+  
+exports.takeTurn = function(channel, Move) {
+    let game = games.filter(game => game.channels.hasOwnProperty(channel))[0];
+    let move = []; // [Move.match(/[0-9]{1,2}/)[0] - 1, 'abcdefghij'.indexOf(Move.toLowerCase().match(/[a-j]/)[0])];
+    let highlight = move;
+      
     // Function will vary with game
     
-    //
- 
-    if (end == 0) game.timer = {
-        time: 100 * 60 * 5,
-        message: "Whoops, it looks like <@" + game.players[game.turn] + "> has run out of time, so the game is over!"
-    }
-     
-    return exports.nextTurn(channel, end, row);
+    // .....
+      
+    exports.nextTurn(channel, end, highlight);
 }
- 
+  
 exports.nextTurn = function(channel, end, highlight) {
-    let game = channels[channel.id];
-    if (end == 0) {
+    let game = games.filter(game => game.channels.hasOwnProperty(channel))[0];
+    if (end == 0)
+    {
         game.turn = game.turn == 0 ? 1 : 0;
         game.player = game.players[game.turn];
+        game.timer = {
+            time: 600,
+            message: `Whoops, it looks like <@${game.players[game.turn]}> has run out of time, so the game is over!`
+        }
     }
-    game.buffer = exports.drawBoard(game, end, highlight);
-    board = new Discord.Attachment(game.buffer, end == 1 ? `${shortname}_${end}_${game.players[game.turn]}.png` : `${shortname}_${end}_${game.players[0]}vs${game.players[1]}.png`);
-    if (channels[channel.id].lastDisplay) channels[channel.id].lastDisplay.delete();
-    return board;
+    else
+    {
+        game.over = true;
+    }
+
+    game.buffer = new Discord.Attachment(exports.drawBoard(game, end, highlight), end == 1 ? `${shortname}_${end}_${game.players[game.winner]}.png` : `${shortname}_${end}_${game.players[0]}vs${game.players[1]}.png`);
+    for (let ch in game.channels)
+    {
+        for (let i = 0; i < game.channels[ch].length; i++)
+        {
+            client.channels.get(ch).messages.get(game.channels[ch][i]).delete();
+        }
+        game.channels[ch] = [];
+    }
+
+    exports.say(game.channels, [end == 0 ? `It is <@${game.player}>'s turn.` : end == 2 ? "Tie game, everyone loses!" : `<@${game.players[game.score[0] > game.score[1] ? 0 : 1]}> has won!`, game.buffer]);
+}
+
+exports.say = function(channels, message) {
+    for (let i in channels)
+    {
+        client.channels.get(i).send(message[0], message[1]);
+    }
+}
+
+// Images
+
+exports.Images = {};
+
+Canvas.loadImage("./img/gameAssets/gamename/board.png").then(image => {
+    exports.Images.board = image;
+});
+
+exports.Images.numbers = new Array(10);
+for (let i = 0; i < 10; i++)
+{
+    Canvas.loadImage(`./img/gameAssets/gamename/numbers/${i}.png`).then(image => {
+        exports.Images.numbers[i] = image;
+    });
 }
