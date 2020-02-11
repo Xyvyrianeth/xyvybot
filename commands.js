f98c6ed11e42bbfa8c4dc97de01a96dd84574726
Xyvyrianeth <strobinsson@gmail.com>
2020-02-10 22:14:12 -0600

    bad markdown

--- commands.js
+++ commands.js
@@ -1,4 +1,4 @@
-var version = "2.45.2.10";
+var version = "2.45.2.11";
 
 const Discord = require("discord.js");
 const Canvas = require("canvas");
@@ -861,7 +861,7 @@ var commands = {
 					embed.setDescription(`<@${player}> does not have a Game History.`);
 				else
 				{
-					let history = [`__\`GAME${" \u200b".repeat(10)}|STATUS|TIME${" \u200b".repeat(13)}|\`\u200b\`REPLAY GIF\`|OPPONENT__\``];
+					let history = [`__\`GAME${" \u200b".repeat(10)}|STATUS|TIME${" \u200b".repeat(13)}|\`\u200b\`REPLAY GIF\`\u200b\`|OPPONENT__\``];
 					res.rows.forEach(match => {
 						gameName = {"othello": "Othello", "squares": "Squares", "rokumoku": "Rokumoku", "ttt3d": "3D Tic Tac Toe", "connect4": "Connect Four", "ordo": "Ordo", "soccer": "Paper Soccer"}[match.game];
 						status = player == match.winner ? "Winner": "Loser \u200b";
