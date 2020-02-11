32249a75024d8cca91ffca9f103e70c6866fae6d
Xyvyrianeth <strobinsson@gmail.com>
2020-02-11 12:51:00 -0600

    Update commands.js

--- commands.js
+++ commands.js
@@ -817,7 +817,7 @@ var commands = {
 						status = player == match.winner ? "Winner": "Loser ";
 						let time = new Date(match.timeStart);
 						time = `${time.getMonth() + 1}/${time.getDate()}/${time.getFullYear().toString().substring(2)} ${time.getHours()}:${time.getMinutes()}`;
-						history.push(`\`${gameName + " \u200b".repeat(14 - gameName.length)}|${status}|${time}|\`[\`OPEN \u200b LINK\`](https://cdn.discordapp.com/attachments/${match.location}/replay_${match.id}.gif)\`|\`<@${match.players[0] == player ? match.players[1] : match.players[0]}>`);
+						history.push(`\`${gameName + " ".repeat(14 - gameName.length)}|${status}|${time}|\`[\`OPEN  LINK\`](https://cdn.discordapp.com/attachments/${match.location}/replay_${match.id}.gif)\`|\`<@${match.players[0] == player ? match.players[1] : match.players[0]}>`);
 					});
 					console.log(history[1].length);
 					console.log(history.join('\n').length);
