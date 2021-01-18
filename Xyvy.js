e87a1503c46e73b71c6ea0190778c34cdfa14982
Xyvyrianeth <strobinsson@gmail.com>
2021-01-17 20:17:03 -0600

    Thought author was the linkable one

--- Xyvy.js
+++ Xyvy.js
@@ -7,7 +7,7 @@ const Discord = require("discord.js"),
 exports.client = client;
 exports.db = db;
 
-var version = package.version + ".4";
+var version = package.version + ".5";
 exports.version = version;
 
 require("/app/assets/prototypes/math.js");
