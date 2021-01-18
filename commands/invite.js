41a65808265cb14f50a2c80c6ecac3da7b23e390
Xyvyrianeth <strobinsson@gmail.com>
2021-01-17 20:20:58 -0600

    ah

--- commands/invite.js
+++ commands/invite.js
@@ -3,7 +3,8 @@ const { db, sqlError } = require("/app/Xyvy.js");
 var { Color } = require("/app/assets/misc/color.js");
 exports.command = (cmd, args, input, message) => {
 	let embed = new Discord.MessageEmbed()
-	.setTitle("Click here to invite me to your server!", "https://discordapp.com/oauth2/authorize?client_id=398606274721480725&scope=bot&permissions=60416")
+	.setTitle("Click here to invite me to your server!")
+	.setURL("https://discordapp.com/oauth2/authorize?client_id=398606274721480725&scope=bot&permissions=60416")
 	.setColor(new Color().random());
 	message.author.send(embed);
 };
